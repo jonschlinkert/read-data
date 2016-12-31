b0086e427b0c306cd964962ae5a7de941e4ba8da
jonschlinkert <jon.schlinkert@sellside.com>
2016-12-30 19:07:32 -0500

    export a function

--- index.js
+++ index.js
@@ -10,99 +10,7 @@
 var fs = require('fs');
 var path = require('path');
 var yaml = require('read-yaml');
-
-/**
- * Asynchronously read a YAML file.
- *
- * ```js
- * var yaml = require('read-data').yaml;
- *
- * yaml('foo.yml', function(err, data) {
- *   if (err) throw err;
- *   console.log(data);
- * });
- * ```
- *
- * @name .yaml
- * @param {String} `fp` path of the file to read.
- * @param {Object|String} `options` to pass to [js-yaml]
- * @param {Function} `cb` callback function
- * @return {Object} JSON
- * @api public
- */
-
-exports.yaml = yaml;
-
-/**
- * Synchronously read a YAML file.
- *
- * ```js
- * var yaml = require('read-data').yaml;
- * var data = yaml.sync('foo.yml');
- * ```
- *
- * @name ..yaml.sync
- * @param {String} `fp` path of the file to read.
- * @param {Object|String} `options` to pass to [js-yaml]
- * @return {Object} JSON
- * @api public
- */
-
-exports.yaml.sync = yaml.sync;
-
-/**
- * Asynchronously read a JSON file.
- *
- * ```js
- * var json = require('read-data');
- *
- * json('foo.json', function(err, data) {
- *   if (err) throw err;
- *   console.log(data);
- * });
- * ```
- *
- * @name .json
- * @param {String} `fp` path of the file to read.
- * @param {Function} `callback` callback function
- * @return {Object} JSON
- * @api public
- */
-
-exports.json = function json(fp, opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts; opts = {};
-  }
-  // opts param exists to maintain the same arity as the
-  // yaml method, so we can dynamically choose the reader
-  fs.readFile(fp, 'utf8', function (err, data) {
-    if (err) cb(err);
-    cb(null, JSON.parse(data));
-  });
-};
-
-/**
- * Synchronously read a JSON file.
- *
- * ```js
- * var json = require('read-data').json;
- * var data = json.sync('foo.json');
- * ```
- *
- * @name .json.sync
- * @param {String} `fp` path of the file to read.
- * @return {Object} JSON
- * @api public
- */
-
-exports.json.sync = function jsonSync(fp) {
-  try {
-    return JSON.parse(fs.readFileSync(fp, 'utf8'));
-  } catch (err) {
-    err.message = 'read-data failed to parse "' + fp + '": ' + err.message;
-    throw err;
-  }
-};
+var extend = require('extend-shallow');
 
 /**
  * Asynchronously read a JSON or YAML file, automatically determining the
@@ -122,36 +30,27 @@ exports.json.sync = function jsonSync(fp) {
  * });
  * ```
  *
- * @name .data
- * @param {String} `fp` path of the file to read.
+ * @name read
+ * @param {String} `filepath` path of the file to read.
  * @param {Object|String} `options` to pass to [js-yaml]
  * @param {Function} `cb` callback function
  * @return {Object} JSON
  * @api public
  */
 
-exports.data = function data(fp, opts, cb) {
-  if (opts && typeof opts === 'function') {
-    cb = opts;
-    opts = {};
+var read = module.exports = function read(filepath, options, cb) {
+  if (options && typeof options === 'function') {
+    cb = options;
+    options = {};
   }
-  opts = opts || {};
-  var ext = opts.lang || path.extname(fp);
-  var reader = exports.json;
-  switch (ext) {
-    case '.json':
-      reader = exports.json;
-      break;
-    case '.yml':
-    case '.yaml':
-      reader = exports.yaml;
-      break;
-  }
-  reader(fp, opts, cb);
+
+  var opts = extend({}, options);
+  var ext = extname(opts.lang || path.extname(filepath));
+  read[ext](filepath, options, cb);
 };
 
 /**
- * Synchronously read a data file, automatically determining the
+ * Synchronously read a `.json` or `.(yaml|yml)` file, automatically determining the
  * reader based on extension.
  *
  * ```js
@@ -161,25 +60,124 @@ exports.data = function data(fp, opts, cb) {
  * var json = data.sync('foo.json');
  * ```
  *
- * @name .data.sync
- * @param {String} `fp` path of the file to read.
+ * @name .sync
+ * @param {String} `filepath` path of the file to read.
  * @param {Object|String} `options` to pass to [js-yaml]
  * @return {Object} JSON
  * @api public
  */
 
-exports.data.sync = function dataSync(fp, opts) {
-  opts = opts || {};
-  var ext = opts.lang || path.extname(fp);
-  var reader = exports.json.sync;
-  switch(ext) {
-    case '.json':
-      reader = exports.json.sync;
-      break;
-    case '.yml':
-    case '.yaml':
-      reader = exports.yaml.sync;
-      break;
-  }
-  return reader(fp, opts);
+read.sync = function(filepath, options) {
+  var opts = extend({}, options);
+  var ext = extname(opts.lang || path.extname(filepath));
+  return read[ext].sync(filepath, options);
 };
+
+/**
+ * Asynchronously read a YAML file.
+ *
+ * ```js
+ * var yaml = require('read-data').yaml;
+ *
+ * yaml('foo.yml', function(err, data) {
+ *   if (err) throw err;
+ *   console.log(data);
+ * });
+ * ```
+ *
+ * @name .yaml
+ * @param {String} `filepath` path of the file to read.
+ * @param {Object|String} `options` to pass to [js-yaml]
+ * @param {Function} `cb` callback function
+ * @return {Object} JSON
+ * @api public
+ */
+
+read.yaml = yaml;
+
+/**
+ * Synchronously read a YAML file.
+ *
+ * ```js
+ * var yaml = require('read-data').yaml;
+ * var data = yaml.sync('foo.yml');
+ * ```
+ *
+ * @name ..yaml.sync
+ * @param {String} `filepath` path of the file to read.
+ * @param {Object|String} `options` to pass to [js-yaml]
+ * @return {Object} JSON
+ * @api public
+ */
+
+read.yaml.sync = yaml.sync;
+
+/**
+ * Asynchronously read a JSON file.
+ *
+ * ```js
+ * var json = require('read-data');
+ *
+ * json('foo.json', function(err, data) {
+ *   if (err) throw err;
+ *   console.log(data);
+ * });
+ * ```
+ *
+ * @name .json
+ * @param {String} `filepath` path of the file to read.
+ * @param {Function} `callback` callback function
+ * @return {Object} JSON
+ * @api public
+ */
+
+read.json = function json(filepath, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+
+  // opts param exists to maintain the same arity as the
+  // yaml method, so we can dynamically choose the reader
+  fs.readFile(filepath, options, function(err, buf) {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    cb(null, JSON.parse(buf.toString()));
+  });
+};
+
+/**
+ * Synchronously read a JSON file.
+ *
+ * ```js
+ * var json = require('read-data').json;
+ * var data = json.sync('foo.json');
+ * ```
+ *
+ * @name .json.sync
+ * @param {String} `filepath` path of the file to read.
+ * @return {Object} JSON
+ * @api public
+ */
+
+read.json.sync = function jsonSync(filepath) {
+  try {
+    return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  } catch (err) {
+    err.message = 'read-data failed to parse "' + filepath + '": ' + err.message;
+    throw err;
+  }
+};
+
+/**
+ * Get the extname without leading `.`
+ */
+
+function extname(ext) {
+  var str = ext.charAt(0) === '.' ? ext.slice(1) : ext;
+  if (str === 'yml') str = 'yaml';
+  return str;
+}
--- test.js
+++ test.js
@@ -5,122 +5,130 @@
  * Licensed under the MIT License.
  */
 
-/* deps:mocha */
-require('should');
-var path = require('path');
+require('mocha');
 var assert = require('assert');
-var YAML = require('js-yaml');
+var yaml = require('js-yaml');
 var read = require('./');
 
-describe('json', function () {
-  describe('.json.sync()', function () {
-    it('should read a json file', function () {
-      read.json.sync('fixtures/test.json').should.be.eql({'a': {'b': 'c'}});
-    });
-  });
+describe('read-data', function() {
+  describe('read', function() {
+    describe('sync', function() {
+      it('should read the YAML file', function() {
+        assert.deepEqual(read.sync('fixtures/test.yaml'), {'a': {'b': 'c', 'd': true}});
+      });
 
-  describe('.json()', function () {
-    it('should read a json file asynchronously', function (done) {
-      read.json('fixtures/test.json', function (err, actual) {
-        actual.should.be.eql({'a': {'b': 'c'}});
-        done();
+      it('should read the JSON file', function() {
+        assert.deepEqual(read.sync('fixtures/test.json'), {'a': {'b': 'c'}});
+      });
+    });
+
+    describe('async', function() {
+      it('should read a JSON file', function(cb) {
+        read('fixtures/test.json', function(err, data) {
+          if (err) return cb(err);
+          assert.deepEqual(data, {'a': {'b': 'c'}});
+          cb();
+        });
+      });
+
+      it('should read a YAML file', function(cb) {
+        read('fixtures/test.yaml', function(err, data) {
+          if (err) return cb(err);
+          assert.deepEqual(data, {'a': {'b': 'c', 'd': true}});
+          cb();
+        });
       });
     });
   });
-});
 
-describe('.yaml.sync()', function () {
-  it('should read the yaml file', function () {
-    read.yaml.sync('fixtures/test.yaml').should.be.eql({'a': {'b': 'c', 'd': true}});
-  });
-});
-
-describe('.yaml()', function () {
-  it('should read the yaml file (async)', function (done) {
-    read.yaml('fixtures/test.yaml', function (err, actual) {
-      actual.should.be.eql({'a': {'b': 'c', 'd': true}});
-      done();
+  describe('.json', function() {
+    describe('sync', function() {
+      it('should read a json file', function() {
+        assert.deepEqual(read.json.sync('fixtures/test.json'), {'a': {'b': 'c'}});
+      });
     });
-  });
-});
 
-describe('.yaml()', function () {
-  it('should read the yaml file asynchronously.', function (done) {
-    read.yaml('fixtures/test.yaml', 'utf8', function (err, actual) {
-      assert.equal(err == null, true);
-      actual.should.eql({'a': {'b': 'c', 'd': true}});
-      done();
-    });
-  });
-  it('should support options.', function (done) {
-    read.yaml('fixtures/test.yaml', {schema: YAML.FAILSAFE_SCHEMA}, function (err, actual) {
-      assert.equal(err == null, true);
-      actual.should.eql({'a': {'b': 'c', 'd': 'true'}});
-      done();
-    });
-  });
-  it('should fail when it cannot parse the file as YAML.', function (done) {
-    read.yaml('index.js', function (err) {
-      err.should.be.an.instanceof(YAML.YAMLException);
-      err.should.have.property('message');
-      err.message.should.containEql('index.js');
-      arguments.should.have.length(1);
-      done();
-    });
-  });
-  it('should fail when it cannot read the data.', function (done) {
-    read.yaml('__a__', function (err) {
-      err.should.be.an.instanceof(Error);
-      err.should.have.property('message');
-      err.message.should.containEql('__a__');
-      arguments.should.have.length(1);
-      done();
-    });
-  });
-});
-
-describe('.yaml.sync()', function () {
-  it('should read the yaml file synchronously.', function () {
-    read.yaml.sync('fixtures/test.yaml', 'utf8').should.eql({'a': {'b': 'c', 'd': true}});
-  });
-  it('should support options.', function () {
-    var actual = read.yaml.sync('fixtures/test.yaml', {schema: YAML.FAILSAFE_SCHEMA});
-    actual.should.eql({'a': {'b': 'c', 'd': 'true'}});
-  });
-  it('should throw an error when it cannot parse the file as YAML.', function () {
-    (function() {
-      read.yaml.sync('README.md', 'utf8');
-    }).should.throw(YAML.YAMLException);
-  });
-  it('should throw an error when it cannot read the data.', function () {
-    (function() {
-      read.yaml.sync('node_modules');
-    }).should.throw(/EISDIR/);
-  });
-});
-
-describe('.data.sync()', function () {
-  it('should read the YAML file automatically', function () {
-    read.data.sync('fixtures/test.yaml').should.be.eql({'a': {'b': 'c', 'd': true}});
-  });
-
-  it('should read the JSON file automatically', function () {
-    read.data.sync('fixtures/test.json').should.be.eql({'a': {'b': 'c'}});
-  });
-});
-
-describe('.data()', function () {
-  it('should read the JSON file automatically (async)', function (done) {
-    read.data('fixtures/test.json', function (err, actual) {
-      actual.should.be.eql({'a': {'b': 'c'}});
-      done();
+    describe('async', function() {
+      it('should read a json file asynchronously', function(cb) {
+        read.json('fixtures/test.json', function(err, data) {
+          if (err) return cb(err);
+          assert.deepEqual(data, {'a': {'b': 'c'}});
+          cb();
+        });
+      });
     });
   });
 
-  it('should read the YAML file automatically (async)', function (done) {
-    read.data('fixtures/test.yaml', function (err, actual) {
-      actual.should.be.eql({'a': {'b': 'c', 'd': true}});
-      done();
+  describe('.yaml', function() {
+    describe('sync', function() {
+      it('should read the yaml file', function() {
+        assert.deepEqual(read.yaml.sync('fixtures/test.yaml'), {'a': {'b': 'c', 'd': true}});
+      });
+
+      it('should read the yaml file synchronously.', function() {
+        assert.deepEqual(read.yaml.sync('fixtures/test.yaml', 'utf8'), {'a': {'b': 'c', 'd': true}});
+      });
+
+      it('should support options.', function() {
+        var data = read.yaml.sync('fixtures/test.yaml', {schema: yaml.FAILSAFE_SCHEMA});
+        assert.deepEqual(data, {'a': {'b': 'c', 'd': 'true'}});
+      });
+
+      it('should throw an error when it cannot parse the file as yaml.', function() {
+        assert.throws(function() {
+          read.yaml.sync('README.md', 'utf8');
+        }, yaml.YAMLException);
+      });
+
+      it('should throw an error when it cannot read the data.', function() {
+        assert.throws(function() {
+          read.yaml.sync('node_modules');
+        }, /EISDIR/);
+      });
+    });
+
+    describe('async', function() {
+      it('should read the yaml file (async)', function(cb) {
+        read.yaml('fixtures/test.yaml', function(err, data) {
+          if (err) return cb(err);
+          assert.deepEqual(data, {'a': {'b': 'c', 'd': true}});
+          cb();
+        });
+      });
+
+      it('should read the yaml file asynchronously.', function(cb) {
+        read.yaml('fixtures/test.yaml', 'utf8', function(err, data) {
+          if (err) return cb(err);
+          assert.deepEqual(data, {'a': {'b': 'c', 'd': true}});
+          cb();
+        });
+      });
+
+      it('should support options.', function(cb) {
+        read.yaml('fixtures/test.yaml', {schema: yaml.FAILSAFE_SCHEMA}, function(err, data) {
+          if (err) return cb(err);
+          assert.deepEqual(data, {'a': {'b': 'c', 'd': 'true'}});
+          cb();
+        });
+      });
+
+      it('should fail when it cannot parse the file as yaml.', function(cb) {
+        read.yaml('index.js', function(err) {
+          assert(err instanceof yaml.YAMLException);
+          assert(err.message);
+          assert(/index\.js/.test(err.message));
+          cb();
+        });
+      });
+
+      it('should fail when it cannot read the data.', function(cb) {
+        read.yaml('__a__', function(err) {
+          assert(err instanceof Error);
+          assert(err.message);
+          assert(/__a__/.test(err.message));
+          cb();
+        });
+      });
     });
   });
 });
