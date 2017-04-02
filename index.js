42d6c1107470311b9a1eff9908bab4beadd6addd
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-02 17:22:26 -0400

    run update, lint-deps

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * read-data <https://github.com/jonschlinkert/read-data>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
